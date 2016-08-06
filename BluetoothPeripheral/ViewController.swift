972bde1985527c9be97dd5e6e7d9a34c8133b281
Jelte Liekens <jelte@wieni.be>
2016-08-06 19:10:02 +0200

    Initial Commit

---
+++ BluetoothPeripheral/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BluetoothPeripheral
+//
+//  Created by Jelte Liekens on 06/08/16.
+//  Copyright © 2016 Jelte Liekens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
